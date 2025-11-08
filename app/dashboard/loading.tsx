3189de8a40fd3a5979efe5f8e1af62d5f15f08ad
Zehan <zehan.kareem@gmail.com>
2025-11-09 00:14:04 +0600

    update(iamzehan): Added loading spinner with animation

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,3 +1,15 @@
-export default function Loading(){
-    return <div>Loading...</div>
+import Icon from "@/app/components/Icons";
+export default function Loading() {
+  return (
+    <div
+      className="
+    flex w-full h-full items-center justify-center"
+    >
+      Loading...
+      <Icon
+        name="DataUsageTwoTone"
+        className="animate-spin text-gray-500 text-2xl"
+      />
+    </div>
+  );
 }
