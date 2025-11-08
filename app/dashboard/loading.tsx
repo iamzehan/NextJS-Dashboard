079a3fdb3bd8cc5f2cafd9c17d1db4d5fca9f7e2
Zehan <zehan.kareem@gmail.com>
2025-11-09 00:16:27 +0600

    update(iamzehan): Added Loader component

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,15 +1,6 @@
-import Icon from "@/app/components/Icons";
+import Loader from "../components/Loader";
 export default function Loading() {
   return (
-    <div
-      className="
-    flex w-full h-full items-center justify-center"
-    >
-      Loading...
-      <Icon
-        name="DataUsageTwoTone"
-        className="animate-spin text-gray-500 text-2xl"
-      />
-    </div>
+    <Loader/>
   );
 }
